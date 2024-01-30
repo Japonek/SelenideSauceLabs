9e1b92453010fff5de5ea82cacc8df586c0eca7a
Igor Klusak <igor.klusak@inetum.com>
2024-01-30 15:57:23 +0100

    update maven yml

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,8 +1,6 @@
 package Pages;
 
 import static com.codeborne.selenide.Selenide.$;
-
-import Tests.User;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.Configuration;
